f1b124ff6561246da4574d8f8a619a48fb7a5ad8
vaibhawp <vaibhawpandey0405@gmail.com>
2024-11-16 13:54:15 +0530

    Devops problem and its solution

---
+++ create pipeline/Deploy-script/scripts/set_permissions.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+chmod +x /home/ubuntu/ECHO/APP/echo-app/scripts/before_install.sh
+chmod +x /home/ubuntu/ECHO/APP/echo-app/scripts/after_install.sh
+chmod +x /home/ubuntu/ECHO/APP/echo-app/scripts/start_application.sh
